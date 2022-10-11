eed306d9bf642303c6210e6d2512276db4e5af74
sherifessam98 <sherifessam1998@gmail.com>
2022-10-11 17:29:10 -0400

    uploading project

---
+++ Assets/Scripts/MainMenuFunction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuFunction : MonoBehaviour {
+
+	public AudioSource buttonPress;
+	public int bestScore;
+	public GameObject bestScoreDisplay;
+
+
+	void Start()
+	{	Cursor.visible = true;
+		bestScore = PlayerPrefs.GetInt("LevelScore");
+		bestScoreDisplay.GetComponent<Text>().text = "BEST: " + bestScore;
+
+	}
+
+	public void PlayGame()
+	{
+		buttonPress.Play();
+		RedirectToLevel.redirectToLevel = 3;
+		SceneManager.LoadScene(2);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void PlayCreds()
+	{
+		buttonPress.Play();
+		SceneManager.LoadScene(4);
+	}
+}
---
+++ Assets/Scripts/RotateSky.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour {
+	
+	public float rotateSpeed = 3.0f;
+
+	void Update () {
+		RenderSettings.skybox.SetFloat ("_Rotation", Time.time * rotateSpeed);
+		
+	}
+}
